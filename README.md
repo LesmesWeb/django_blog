db62728bb25550fa802af18bae166ecccd42a928
Dev-Lesmes <lesmes01@hotmail.com>
2019-11-28 23:57:16 -0500

    Creación de modelos

--- README.md
+++ README.md
@@ -6,10 +6,12 @@
 2. cd django_blog
    2.1 - mkdir templates
    2.2 - cd templates
-   2.3 - \$echo "" >> **init**.py
+   2.3 - \$echo "" >> __init__.py
 3. mkdir apps
    3.2 - cd apps
-   3.3 - \$echo "" >> **init**.py
-   3.4 - django-admin startapp libro
-4. 'apps.libro', #registramos el app en settings.py INSTALLED_APPS
+   3.3 - $echo "" >> __init__.py
+   3.4 - django-admin startapp blog
+4. 'apps.blog', #registramos el app en settings.py INSTALLED_APPS
 
+5. apps/blog/models.py --> Creación la base de datos en el modelo 
+6. apps/blog/admin.py --> Mostramos los campos del modelo en el admin de Django
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField('Nombre de la Categoria', max_length=100,null=False,blank=False)
+    estado = models.BooleanField('Categoria Activada/Categoria no Activada',default=True)
+    #auto_now = True actualiza el campo cada vez que se actualice el modelo, auto_now_add = True añadirlo solo una vez
+    fecha_creacion = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
+   
+
+    class Meta:
+        verbose_name = 'Categoria' #manera que vamos a indeficar el modelo en el admin
+        verbose_name_plural = 'Categorias'
+
+    def __str__(self):
+        return self.nombre
+
+class Autor(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombres = models.CharField('Nombres de Autor', max_length=255,blank=False,null=False)
+    apellidos = models.CharField('Apellidos de Autor', max_length=255,blank=False,null=False)
+    facebook = models.URLField('Facebook',null=True,blank=True)
+    twitter = models.URLField('Twitter',null=True,blank=True)
+    instagram = models.URLField('Instagram',null=True,blank=True)
+    web = models.URLField('Web',null=True,blank=True)
+    coreo = models.EmailField('Correo Electronico',blank=False,null=False)
+    estado = models.BooleanField('Autor Activo/No Activo',default=True)
+    #auto_now = True actualiza el campo cada vez que se actualice el modelo, auto_now_add = True añadirlo solo una vez
+    fecha_creación = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return "{0},{1}".format({self.apellidos,self.nombres})
+    
