bb6995936c7b0ee53eb86f27767f7042df8ab024
Dev-Lesmes <lesmes01@hotmail.com>
2019-12-16 00:34:11 -0500

    Video 30 - Post detallle

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -5,8 +5,13 @@ from .models import Post,Categoria
 def home(request):
     posts = Post.objects.filter(estado=True)
     print(posts)
-
     return render(request,'index.html',{'posts':posts})
+
+def detallePost(request,slug):
+    post = Post.objects.get(
+        slug = slug
+    )
+    return render(request,'post.html',{'detalle_post':post})
 
 def generales(request):
     posts = Post.objects.filter(
