d02ad3b5cdda53ea2b435c00986806404b23a1c5
Dev-Lesmes <lesmes01@hotmail.com>
2019-12-16 02:21:33 -0500

    Video 32 - Barra de busqueda general y especifica

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 from .models import Post,Categoria
 from django.shortcuts import get_object_or_404
 from django.http import Http404
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
+    queryset = request.GET.get("buscar")
     posts = Post.objects.filter(estado=True)
-    print(posts)
+
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo__icontains = queryset) |  #FILTRO POR O
+            Q(descripcion__icontains = queryset) #icontains no importa si hay mayusculas o minusculas
+        ).distinct()#distinct trae los que no son iguales
+
     return render(request,'index.html',{'posts':posts})
 
 def detallePost(request,slug):
@@ -16,19 +24,29 @@ def detallePost(request,slug):
 
 def generales(request):
     #validación mas detallada
+    
     try:
+        queryset = request.GET.get("buscar")
         posts = Post.objects.filter(
             estado=True,
             categoria=Categoria.objects.get(nombre__iexact='general') #__exact especifica que el campo debe ser identico
         )
 
+        if queryset:
+            posts=Post.objects.filter(
+                Q(titulo__icontains = queryset) |  #FILTRO POR O
+                Q(descripcion__icontains = queryset), #icontains no importa si hay mayusculas o minusculas
+                estado=True,
+                categoria=Categoria.objects.get(nombre__iexact='general'),
+
+        ).distinct()#distinct trae los que no son iguales
     except Post.DoesNotExist:
         raise Http404("No existe la Post correcto")
 
     except Categoria.DoesNotExist:
         raise Http404("No existe la Categoria correcto")
 
-    return render(request,'generales.html',{'posts':posts})
+    return render(request,'generales.html',{'posts':posts}) 
 
 def programacion(request):
     posts = Post.objects.filter(
