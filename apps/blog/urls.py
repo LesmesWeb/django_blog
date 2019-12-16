f43c42f14ea7b1e73039039e1cd604e24bf684ee
Dev-Lesmes <lesmes01@hotmail.com>
2019-12-16 00:59:13 -0500

    Video 31 - Validaciones try 404 y iexact

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,10 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('',home,name='index'),
-    path('<slug:slug>/',detallePost, name='detalle_post'),
     path('generales/',generales,name='generales'),
     path('programacion/',programacion,name='programacion'),
     path('tutoriales/',tutoriales,name='tutoriales'),
     path('tecnologia/',tecnologia,name='tecnologia'),
     path('videojuegos/',videojuegos,name='videojuegos'),
+    #el orden en la url influye debido a que si hay un parametro en la parte inicial lo solicitara
+    path('<slug:slug>/',detallePost, name='detalle_post'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post,Categoria
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 # Create your views here.
 def home(request):
@@ -8,42 +10,50 @@ def home(request):
     return render(request,'index.html',{'posts':posts})
 
 def detallePost(request,slug):
-    post = Post.objects.get(
-        slug = slug
-    )
+    #Validación de 404 manera agil
+    post = get_object_or_404(Post,slug=slug)
     return render(request,'post.html',{'detalle_post':post})
 
 def generales(request):
-    posts = Post.objects.filter(
-        estado=True,
-        categoria=Categoria.objects.get(nombre='General')
-    )
+    #validación mas detallada
+    try:
+        posts = Post.objects.filter(
+            estado=True,
+            categoria=Categoria.objects.get(nombre__iexact='general') #__exact especifica que el campo debe ser identico
+        )
+
+    except Post.DoesNotExist:
+        raise Http404("No existe la Post correcto")
+
+    except Categoria.DoesNotExist:
+        raise Http404("No existe la Categoria correcto")
+
     return render(request,'generales.html',{'posts':posts})
 
 def programacion(request):
     posts = Post.objects.filter(
         estado=True,
-        categoria=Categoria.objects.get(nombre='Programación')
+        categoria=Categoria.objects.get(nombre__iexact='Programación')
     )
     return render(request,'programacion.html',{'posts':posts})
 
 def tutoriales(request):
     posts = Post.objects.filter(
         estado=True,
-        categoria=Categoria.objects.get(nombre='Tutoriales')
+        categoria=Categoria.objects.get(nombre__iexact='Tutoriales')
     )
     return render(request,'tutoriales.html',{'posts':posts})
 
 def tecnologia(request):
     posts = Post.objects.filter(
         estado=True,
-        categoria=Categoria.objects.get(nombre='Tecnología')
+        categoria=Categoria.objects.get(nombre__iexact='Tecnología')
     )
     return render(request,'tecnologia.html',{'posts':posts})
 
 def videojuegos(request):
     posts = Post.objects.filter(
         estado=True,
-        categoria=Categoria.objects.get(nombre='Videojuegos')
+        categoria=Categoria.objects.get(nombre__iexact='Videojuegos')
     )
     return render(request,'videojuegos.html',{'posts':posts})
