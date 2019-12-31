b185c1f98218b208ff17383a05928da7c273a097
Dev-Lesmes <lesmes01@hotmail.com>
2019-12-30 23:23:48 -0500

    Video 33 - paginación Finalizada

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -3,6 +3,8 @@ from .models import Post,Categoria
 from django.shortcuts import get_object_or_404
 from django.http import Http404
 from django.db.models import Q
+#Paginación
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
@@ -14,6 +16,13 @@ def home(request):
             Q(titulo__icontains = queryset) |  #FILTRO POR O
             Q(descripcion__icontains = queryset) #icontains no importa si hay mayusculas o minusculas
         ).distinct()#distinct trae los que no son iguales
+
+    #Paginator recibe dos parametros el primero es la lista (Post), y la paginación
+    paginator = Paginator(posts,2)
+    #obtiene la pagina en la cuestión actual
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
 
     return render(request,'index.html',{'posts':posts})
 
@@ -49,29 +58,97 @@ def generales(request):
     return render(request,'generales.html',{'posts':posts}) 
 
 def programacion(request):
-    posts = Post.objects.filter(
-        estado=True,
-        categoria=Categoria.objects.get(nombre__iexact='Programación')
-    )
+    try:
+        queryset = request.GET.get("buscar")
+        posts = Post.objects.filter(
+            estado=True,
+            categoria=Categoria.objects.get(nombre__iexact='Programación') #__exact especifica que el campo debe ser identico
+        )
+
+        if queryset:
+            posts=Post.objects.filter(
+                Q(titulo__icontains = queryset) |  #FILTRO POR O
+                Q(descripcion__icontains = queryset), #icontains no importa si hay mayusculas o minusculas
+                estado=True,
+                categoria=Categoria.objects.get(nombre__iexact='Programación'),
+
+        ).distinct()#distinct trae los que no son iguales
+    except Post.DoesNotExist:
+        raise Http404("No existe la Post correcto")
+
+    except Categoria.DoesNotExist:
+        raise Http404("No existe la Categoria correcto")
+
     return render(request,'programacion.html',{'posts':posts})
 
 def tutoriales(request):
-    posts = Post.objects.filter(
-        estado=True,
-        categoria=Categoria.objects.get(nombre__iexact='Tutoriales')
-    )
+    try:
+        queryset = request.GET.get("buscar")
+        posts = Post.objects.filter(
+            estado=True,
+            categoria=Categoria.objects.get(nombre__iexact='Tutoriales') #__exact especifica que el campo debe ser identico
+        )
+
+        if queryset:
+            posts=Post.objects.filter(
+                Q(titulo__icontains = queryset) |  #FILTRO POR O
+                Q(descripcion__icontains = queryset), #icontains no importa si hay mayusculas o minusculas
+                estado=True,
+                categoria=Categoria.objects.get(nombre__iexact='Tutoriales'),
+
+        ).distinct()#distinct trae los que no son iguales
+    except Post.DoesNotExist:
+        raise Http404("No existe la Post Tutoriales correcto")
+
+    except Categoria.DoesNotExist:
+        raise Http404("No existe la Categoria Tutoriales correcto")
+
     return render(request,'tutoriales.html',{'posts':posts})
 
 def tecnologia(request):
-    posts = Post.objects.filter(
-        estado=True,
-        categoria=Categoria.objects.get(nombre__iexact='Tecnología')
-    )
+    try:
+        queryset = request.GET.get("buscar")
+        posts = Post.objects.filter(
+            estado=True,
+            categoria=Categoria.objects.get(nombre__iexact='Tecnología') #__exact especifica que el campo debe ser identico
+        )
+
+        if queryset:
+            posts=Post.objects.filter(
+                Q(titulo__icontains = queryset) |  #FILTRO POR O
+                Q(descripcion__icontains = queryset), #icontains no importa si hay mayusculas o minusculas
+                estado=True,
+                categoria=Categoria.objects.get(nombre__iexact='Tecnología'),
+
+        ).distinct()#distinct trae los que no son iguales
+    except Post.DoesNotExist:
+        raise Http404("No existe la Post Tutoriales correcto")
+
+    except Categoria.DoesNotExist:
+        raise Http404("No existe la Categoria Tutoriales correcto")
+
     return render(request,'tecnologia.html',{'posts':posts})
 
 def videojuegos(request):
-    posts = Post.objects.filter(
-        estado=True,
-        categoria=Categoria.objects.get(nombre__iexact='Videojuegos')
-    )
+    try:
+        queryset = request.GET.get("buscar")
+        posts = Post.objects.filter(
+            estado=True,
+            categoria=Categoria.objects.get(nombre__iexact='Videojuegos') #__exact especifica que el campo debe ser identico
+        )
+
+        if queryset:
+            posts=Post.objects.filter(
+                Q(titulo__icontains = queryset) |  #FILTRO POR O
+                Q(descripcion__icontains = queryset), #icontains no importa si hay mayusculas o minusculas
+                estado=True,
+                categoria=Categoria.objects.get(nombre__iexact='Videojuegos'),
+
+        ).distinct()#distinct trae los que no son iguales
+    except Post.DoesNotExist:
+        raise Http404("No existe la Post Tutoriales correcto")
+
+    except Categoria.DoesNotExist:
+        raise Http404("No existe la Categoria Tutoriales correcto")
+
     return render(request,'videojuegos.html',{'posts':posts})
